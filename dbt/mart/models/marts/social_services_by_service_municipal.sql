9e416f342995a30a430b5d09d4977612a6ee2b87
Vicc <vmchura@gmail.com>
2025-09-27 12:59:48 +0200

    using external location

--- dbt/mart/models/marts/social_services_by_service_municipal.sql
+++ dbt/mart/models/marts/social_services_by_service_municipal.sql
@@ -1,5 +1,6 @@
 {{ config(
-    partitioned_by=['downloaded_date']
+    partitioned_by=['downloaded_date'],
+    external_location="s3://{{ var('data_bucket') }}/marts/social_services_by_service_municipal"
 ) }}
 
 SELECT
