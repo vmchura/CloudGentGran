ff072b13eb15fd9530b793a74e48f965fb2326fe
Vicc <vmchura@gmail.com>
2025-11-09 12:57:33 +0100

    observable with no pages

--- observable/observablehq.config.js
+++ observable/observablehq.config.js
@@ -6,12 +6,6 @@ export default {
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
-  pages: [
-    {
-      name: "Gent-Gran", path: "/projects/gent-gran/index.html",
-    }
-  ],
-
   // Content to add to the head of the page, e.g. for a favicon:
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
 
