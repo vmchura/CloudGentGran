9111b92457c35f49f184b465f8a04f8fa7809b17
Vicc <vmchura@gmail.com>
2026-01-25 12:09:04 +0100

    reading from dbt

--- dbt/mart/macros/read_staging_data.sql
+++ dbt/mart/macros/read_staging_data.sql
@@ -1,7 +1,7 @@
 {% macro read_staging_data(table_name, partition_key, partition_value) %}
     {% if target.name == "local" %}
-        -- DuckDB: Read directly from parquet files
-        read_parquet('{{ var("data_bucket") }}/staging/{{ table_name }}/{{ partition_key }}={{ partition_value }}/*.parquet')
+        -- DuckDB: Read directly from parquet files s3
+        read_parquet('s3://{{ var("data_bucket") }}/staging/{{ table_name }}/{{ partition_key }}={{ partition_value }}/*.parquet')
     {% else %}
         -- Athena: Use source table with partition filter
         {{ source('staging', table_name) }}
