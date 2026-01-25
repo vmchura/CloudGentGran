94f3a9f26c42f66fe5520ce81d284015212a84f5
Vicc <vmchura@gmail.com>
2026-01-25 15:47:16 +0100

    works writing in s3 duckdb and dbt

--- dbt/mart/models/marts/social_services_by_service_municipal.sql
+++ dbt/mart/models/marts/social_services_by_service_municipal.sql
@@ -1,15 +1,21 @@
+{{config(materialized='external',
+format = 'parquet',
+location = "s3://{{ env_var('DATA_BUCKET') }}/marts/{{this.name}}",
+options = { "per_thread_output" : true }) }}
 SELECT
     service_qualification_id,
     service_type_id,
     comarca_id,
     municipal_id,
-    EXTRACT(YEAR FROM inscription_date) as year,
-    EXTRACT(MONTH FROM inscription_date) as month,
-    SUM(capacity) as total_capacit
-FROM {{ read_staging_data('social_services', 'downloaded_date', var('downloaded_date')) }}
-WHERE downloaded_date = '{{ var("downloaded_date") }}' and
-    capacity > 0 and
-    service_type_id in ('DAY-001', 'RES-003', 'RES-002', 'TUT-001', 'RES-001')
+    EXTRACT(YEAR FROM inscription_date) AS year,
+    EXTRACT(MONTH FROM inscription_date) AS month,
+    SUM(capacity) AS total_capacit
+FROM
+    {{ read_staging_data ('social_services', 'downloaded_date', var ('downloaded_date')) }}
+WHERE
+    downloaded_date = '{{ var("downloaded_date") }}'
+    AND capacity > 0
+    AND service_type_id IN ('DAY-001', 'RES-003', 'RES-002', 'TUT-001', 'RES-001')
 GROUP BY
     service_qualification_id,
     service_type_id,
@@ -17,3 +23,4 @@ GROUP BY
     municipal_id,
     EXTRACT(YEAR FROM inscription_date),
     EXTRACT(MONTH FROM inscription_date)
+
