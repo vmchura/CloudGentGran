56834855f334ec029f6684866c04984b5e7d33c5
Vicc <vmchura@gmail.com>
2026-01-28 19:18:21 +0100

    read directly macro in duckdb

--- dbt/mart/macros/read_catalog_data.sql
+++ dbt/mart/macros/read_catalog_data.sql
@@ -1,6 +1,6 @@
 {% macro read_catalog_data(table_name) %}
     {% if target.name == "local" %}
-        read_parquet('s3://{{ var("catalog_bucket") }}/{{ table_name }}/*.parquet')
+        read_parquet('s3://{{ var("catalog_bucket") }}/{{ table_name }}/{{ table_name }}.parquet')
     {% else %}
         {{ source('catalog', table_name) }}
     {% endif %}
