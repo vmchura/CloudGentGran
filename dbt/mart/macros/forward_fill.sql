2b819113643c28def9fd9ebc6289a71c93cd40f0
Vicc <vmchura@gmail.com>
2026-01-31 23:14:22 +0100

    fixed forward fill for athena, alternative

--- dbt/mart/macros/forward_fill.sql
+++ dbt/mart/macros/forward_fill.sql
@@ -10,7 +10,11 @@
 
 {% macro athena__forward_fill(expr, partition_by, order_by) %}
   max_by({{ expr }}, {{ order_by }})
-  OVER (PARTITION BY {{ partition_by }} ORDER BY {{ order_by }}
-        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
+  FILTER (WHERE {{ expr }} IS NOT NULL)
+  OVER (
+    PARTITION BY {{ partition_by }}
+    ORDER BY {{ order_by }}
+    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+  )
 {% endmacro %}
 
