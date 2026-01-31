2c00ec94e891d525241423d337c4505eec54eb40
Vicc <vmchura@gmail.com>
2026-01-31 23:48:34 +0100

    Revert "another iteration forwardfill for athena"
    This reverts commit 4838c92082ac9fc9fcf1097d9fa54f370a9d2a30.

--- dbt/mart/macros/forward_fill.sql
+++ dbt/mart/macros/forward_fill.sql
@@ -9,27 +9,12 @@
 {% endmacro %}
 
 {% macro athena__forward_fill(expr, partition_by, order_by) %}
-
-MAX(
-  CASE
-    WHEN {{ order_by }} = 
-      MAX(
-        CASE
-          WHEN {{ expr }} IS NOT NULL THEN {{ order_by }}
-        END
-      ) OVER (
-        PARTITION BY {{ partition_by }}
-        ORDER BY {{ order_by }}
-        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-      )
-    THEN {{ expr }}
-  END
-)
-OVER (
-  PARTITION BY {{ partition_by }}
-  ORDER BY {{ order_by }}
-  ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
-)
-
+  max_by({{ expr }}, {{ order_by }})
+  FILTER (WHERE {{ expr }} IS NOT NULL)
+  OVER (
+    PARTITION BY {{ partition_by }}
+    ORDER BY {{ order_by }}
+    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+  )
 {% endmacro %}
 
