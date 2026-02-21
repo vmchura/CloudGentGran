88c48e9b91a68e50cfecc99077c20338b4951a6b
Vicc <vmchura@gmail.com>
2026-02-21 16:51:44 +0100

    imputation of qualification id for specific entities

--- dbt/mart/models/marts/social_services_by_service_municipal.sql
+++ dbt/mart/models/marts/social_services_by_service_municipal.sql
@@ -1,13 +1,14 @@
-{{ adapter_aware_table_config () }} WITH social_services AS (
+{{ adapter_aware_table_config () }}
+WITH social_services AS (
     SELECT
-	social_service_register_id,
+        social_service_register_id,
         service_qualification_id,
         service_type_id,
         comarca_id,
         municipal_id,
         EXTRACT(YEAR FROM inscription_date) AS year,
         EXTRACT(MONTH FROM inscription_date) AS month,
-        SUM(capacity) AS total_capacit
+        capacity
     FROM
         {{ read_staging_data ('social_services', 'downloaded_date', var ('downloaded_date')) }}
     WHERE
@@ -21,15 +22,21 @@ social_services_with_input_qualification AS (
             CASE WHEN service_qualification_id IS NULL THEN
                 CASE social_service_register_id
                 WHEN 'S07873' THEN
-                    -- S07873 Residència assistida Can Serra: Asproseat grup és un conjunt d’entitats socials d’àmbit català que té com a finalitat principal
+                    -- S07873 Residència assistida Can Serra: És un servei d'ASPROSEAT: Asproseat grup és un conjunt d’entitats socials d’àmbit català que té com a finalitat principal
+                    'PRV-002'
+                WHEN 'S07874' THEN
+                    -- S07874 Centre de dia Can Serra: És un servei d'ASPROSEAT, Asproseat grup és un conjunt d’entitats socials d’àmbit català que té com a finalitat principal
                     'PRV-002'
                 WHEN 'S10642' THEN
                     -- S10642 Residència de gent gran El Tossalet: El Tossalet és una Residència i Centre de dia per a persones grans, situada a Lleida, a Cervià de les Garrigues (Lleida) gestionat per la Fundació Persona i Valors des del 2021. És un centre especialitzat en persones grans amb necessitats específiques.
                     'PRV-002'
+                WHEN 'S10643' THEN
+                    -- S10643 Centre de dia El Tossalet: El Tossalet és una Residència i Centre de dia per a persones grans, situada a Lleida, a Cervià de les Garrigues (Lleida) gestionat per la Fundació Persona i Valors des del 2021. És un centre especialitzat en persones grans amb necessitats específiques.
+                    'PRV-002'
                 WHEN 'S10486' THEN
                     -- S10486 Residència Seniors Ceritània: Clariane es la primera red europea de residencias para las personas de la tercera edad con clínicas especializadas, centros asistenciales con distintas modalidades y hospitalización a domicilio. Como empresa comprometida con el servicio asistencial
                     'PRV-001'
-                WHEN 'S10486' THEN
+                WHEN 'S07316' THEN
                     -- S07316 Residencia Idea Vilapicina: Juntos definieron un propósito corporativo diferenciador: mejorar la calidad de vida de las personas con un enfoque ético de los cuidados.
                     'PRV-001'
                 END
@@ -41,12 +48,18 @@ social_services_with_input_qualification AS (
         municipal_id,
         year,
         month,
-        total_capacit
+        capacity
     FROM
         social_services
 )
 SELECT
-    *
+    service_qualification_id,
+    service_type_id,
+    comarca_id,
+    municipal_id,
+    year,
+    month,
+    SUM(capacity) as total_capacit
 FROM
     social_services_with_input_qualification
 GROUP BY
@@ -54,5 +67,5 @@ GROUP BY
     service_type_id,
     comarca_id,
     municipal_id,
-    EXTRACT(YEAR FROM inscription_date),
-    EXTRACT(MONTH FROM inscription_date)
+    year,
+    month
