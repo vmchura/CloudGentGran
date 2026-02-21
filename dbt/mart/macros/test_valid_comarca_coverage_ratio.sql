bf1ed366c419fd3b5d0821cfa4804e6f53c5ab32
Vicc <vmchura@gmail.com>
2026-02-21 11:04:19 +0100

    improve test

--- dbt/mart/macros/test_valid_comarca_coverage_ratio.sql
+++ dbt/mart/macros/test_valid_comarca_coverage_ratio.sql
@@ -4,12 +4,12 @@ SELECT
     comarca_id,
     year,
     coverage_ratio,
-    population_ge65,
+    population_age_65_and_over,
     total_capacit
 FROM {{ model }}
 WHERE
     coverage_ratio < 0
     OR coverage_ratio > 100
-    OR (total_capacit > 0 AND population_ge65 <= 0)
+    OR (total_capacit > 0 AND population_age_65_and_over <= 0)
 
 {% endtest %}
